89f4e626640bb8b3b825a04dffbc1ee07eeaa589
Kevin Huang <mail2yizhou@gmail.com>
2019-06-05 16:28:57 +1000

    setup program from vscode

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_Geometry_Caculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
